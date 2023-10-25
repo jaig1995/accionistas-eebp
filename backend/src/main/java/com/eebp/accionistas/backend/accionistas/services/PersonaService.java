c95e49a0869e6a6c238f4d31111957c624036914
Johnathan Insuasty <johnathan.insuasty@nordstrom.com>
2023-10-25 01:01:02 -0500

    Fix formato

--- backend/src/main/java/com/eebp/accionistas/backend/accionistas/services/PersonaService.java
+++ backend/src/main/java/com/eebp/accionistas/backend/accionistas/services/PersonaService.java
@@ -145,7 +145,7 @@ public class PersonaService {
         if (!datosPersona.getTipDocumento().equalsIgnoreCase("TI")) {
 
             document.selectFirst("#dirLaboral").text(datosPersona.getDirLaboral());
-            if(datosPersona.getMunicipioLaboral().isEmpty()) {
+            if(!datosPersona.getMunicipioLaboral().isEmpty()) {
                 document.selectFirst("#munLaboral").text(municipioRepository.findById(Integer.parseInt(datosPersona.getMunicipioLaboral())).get().getNombreMunicipio().toUpperCase());
                 document.selectFirst("#depLaboral").text(municipioRepository.findById(Integer.parseInt(datosPersona.getMunicipioLaboral())).get().getDepartamento().getNombreDepartamento().toUpperCase());
             }
